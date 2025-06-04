72e37c8d2fdc892690097926822c2662c49c8831
Aditya Katoch <akatoch366@gmail.com>
2025-06-04 15:45:47 +0530

    fix: nav

--- script.js
+++ script.js
@@ -133,3 +133,19 @@ document.querySelectorAll(".callback").forEach(function (element) {
     window.open("https://packages.infinitehospitality.in", "_self");
   });
 });
+
+
+// navbar-scroll.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.getElementById("navbar");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      navbar.classList.add("bg-zinc-500/70", "backdrop-blur-md", "shadow-md");
+    } else {
+      navbar.classList.remove("bg-zinc-500/70", "backdrop-blur-md", "shadow-md");
+    }
+  });
+});
+
